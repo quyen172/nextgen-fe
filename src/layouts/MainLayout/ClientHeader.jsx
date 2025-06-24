381db5da0bfcc23065aa33859183e0d55bf662b2
La Văn Quyến <lavanquyen416@gmail.com>
2025-06-25 00:24:49 +0700

    update template

--- src/layouts/MainLayout/ClientHeader.jsx
+++ src/layouts/MainLayout/ClientHeader.jsx
@@ -16,50 +16,56 @@ const ClientHeader = () => {
       {/* Navbar */}
       <div className="site-navbar-wrap">
         <div className="site-navbar">
-          <div className="container py-1">
-            <div className="row align-items-center">
-              <div className="col-2">
-                <h1 className="mb-0 site-logo">
-                  <a href="/">NextGen</a>
-                </h1>
+          <div className="container tw-py-6 tw-flex tw-items-center tw-justify-between">
+            <Link to="/" className="!tw-text-white tw-text-4xl tw-font-normal">
+              Interior
+            </Link>
+
+            <form
+              action=""
+              className="tw-w-[300px] tw-h-12 tw-border tw-border-solid tw-border-[#BDBDBD] tw-flex"
+            >
+              <input
+                type="text"
+                placeholder="Search"
+                className="tw-bg-transparent tw-border-none tw-outline-none tw-flex-1 tw-px-2 tw-text-white [&::placeholder]:tw-text-gray-[#BDBDBD]"
+              />
+
+              <div className="tw-text-white tw-self-center tw-mx-4">
+                <i className="fa-solid fa-magnifying-glass"></i>
               </div>
-              <div className="col-10">
-                <nav className="site-navigation text-right" role="navigation">
-                  <div className="container">
-                    <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
-                      <a
-                        href="#"
-                        className="site-menu-toggle js-menu-toggle text-white"
-                      >
-                        <span className="icon-menu h3"></span>
-                      </a>
-                    </div>
-                    <ul className="site-menu js-clone-nav d-none d-lg-block">
-                      <li>
-                        <NavLink to="/">Home</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/about">About Us</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/projects">Projects</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/news">News</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/services">Services</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/contact">Contact</NavLink>
-                      </li>
-                      <li>
-                        <NavLink to="/admin">Admin</NavLink>
-                      </li>
-                    </ul>
-                  </div>
-                </nav>
+            </form>
+
+            <ul className="tw-flex tw-items-center tw-gap-6 tw-list-none tw-m-0">
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About Us</NavLink>
+              </li>
+              <li>
+                <NavLink to="/projects">Projects</NavLink>
+              </li>
+              <li>
+                <NavLink to="/news">News</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contact">Contact</NavLink>
+              </li>
+            </ul>
+
+            <div className="tw-flex tw-items-center tw-gap-6">
+              <div className="tw-text-white tw-text-xl">
+                <i class="fa-regular fa-heart"></i>
               </div>
+
+              <div className="tw-text-white tw-text-xl">
+                <i class="fa-regular fa-user"></i>
+              </div>
+
+              <Link to="/cart" className="tw-text-white tw-text-xl">
+                <i class="fa-solid fa-cart-shopping"></i>
+              </Link>
             </div>
           </div>
         </div>
